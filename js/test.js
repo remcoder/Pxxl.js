50288a3c3a92d9938bd8a4bf68dd5697a77a2e5f
Remco Veldkamp <remcoder@gmail.com>
2014-01-22 22:39:43 +0100

    cleaned up and commented example

--- js/test.js
+++ js/test.js
@@ -1,24 +1,25 @@
 var pxxl = require("./utils.js");
 
+// ascii art demo
 pxxl('../fonts/c64.bdf', 'Hello World', function(text, pixels, font) {
-	var buffer = [];
-	//console.log(pixels);
 
+	// initialize buffer, 8 columns wide for each character
+	var buffer = [];
 	for(var line=0 ; line<8; line++)
 		buffer.push(new Array(text.length*8).join("."));
 
+	// draw pixels to buffer
 	for (var i=0 ; i<pixels.length ; i++)
 	{
 		var p = pixels[i];
-		// console.log(typeof buffer[p.y]);
-		// console.log(buffer[p.y][p.x]);
+
+		// for every pixel, replace a character in the buffer
 		var tmp=buffer[p.y].split("");
 		tmp.splice(p.x,1,"#");
-
 		buffer[p.y] = tmp.join("");
 	}
 	
+	// write buffer to screen
 	for(var line=0 ; line<8; line++)
 		console.log(buffer[line]);
-
 });
