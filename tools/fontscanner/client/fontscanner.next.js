fb846dcf08406a7f42e1922769e6ffa110133f84
Remco Veldkamp <remcoder@gmail.com>
2014-08-06 22:19:22 +0200

    added css animation to the svg to prove the point. added bdf output for individual glyphs.

--- tools/fontscanner/client/fontscanner.next.js
+++ tools/fontscanner/client/fontscanner.next.js
@@ -12,7 +12,7 @@ dummy.src = 'BigDots.woff'
 Meteor.startup(function() {
 	var ascii = _.range(33, 127)
       .map( code => String.fromCharCode(code) ).join("");
-     Session.set('text', ascii);
+     Session.set('text', "a");
 })
 
 Template.scanner.events({
@@ -39,28 +39,63 @@ Template.scanner.helpers({
   }
 });  
 
+var pixels = Blaze.ReactiveVar([]);
+
+Template.glyph.helpers({
+	encoding : function () { return this.charCodeAt(0); },
+	bytes : function () { 
+
+		return pixels.get().map(row => {
+			var left = row.slice(0,8);
+			var right = row.slice(8);
+
+			var bit = 128;
+			var resultLeft = 0;
+			for(var i=0 ; i<8 ; i++) {
+				if( left[i] )
+					resultLeft |= bit;
+				bit >>>= 1;
+			}
+
+			var bit = 128;
+			var resultRight = 0;
+			for(var i=0 ; i<8 ; i++) {
+				if( right[i] )
+					resultRight |= bit;
+				bit >>>= 1;
+			}
+
+			return _.str.pad(resultLeft.toString(16).toUpperCase(),2,"0") + 
+				_.str.pad(resultRight.toString(16).toUpperCase(),2,"0");
+		});
+	}
+});
+
+
+
 Template.glyph.rendered = function ()  {
 	var canvas = this.find('canvas');
 	var svg = this.find('svg');
+	var pre = this.find('pre');
 	var ctx=canvas.getContext("2d");
 
 	ctx.fillStyle = "#ddd";
 	for(var y=0 ; y<16 ; y++)
 	for(var x=0 ; x<16 ; x++)
 		if( (x+y) % 2 )
-			ctx.fillRect(x*16,y*16,16,16);
+			ctx.fillRect(x*16,y*16,14,16);
 	
-
 	ctx.font="normal 256px BigDots";
-	ctx.fillStyle = "black"	;
-	this.autorun(() => {
-		fontFile.depend();
+	ctx.fillStyle = "rgba(0,0,0,1)";
+	
+	this.autorun(() => { 	// create reactive code
+		fontFile.depend();  // ensure the reactive code is run again when the fontFile dep changes
 		if (fontFileLoaded) {
 			Meteor.setTimeout(()=> {
 				ctx.fillText(this.data.split("") ,0, 256-16*3 - 2);
-				var glyph = scan(canvas);
-				// console.log(glyph);
-				build(svg, glyph);
+				var pixelData = scan(canvas);
+				pixels.set(pixelData);
+				build(svg, pixels);
 			},0);
 		}
 		// console.log('autorun')
@@ -78,7 +113,7 @@ function scan(canvas) {
 			var cx=x+7, cy=y+7;
 			var data = ctx.getImageData(cx, cy, 1,1).data;
 
-			if (data[0] == 0 && data[3] == 255) {
+			if (data[0] == 0 && data[3] > 0) {
 				ctx.fillStyle = "rgba(255,0,0,0.5)";
 				ctx.fillRect(x,y,16,16);
 				row.push(1);
@@ -97,9 +132,10 @@ function makeSVG(tag, attrs) {
     return el;
 }
 
-function build(svg, glyph) {
+function build(svg, pixels) {
 	// console.log(svg);
 	$(svg).empty();
+	var glyph = pixels.get();
 
 	for(var y=0 ; y<16 ; y++)
 	for(var x=0 ; x<16 ; x++) {
@@ -117,4 +153,5 @@ function build(svg, glyph) {
 		}
 	}	
 }
+
 
